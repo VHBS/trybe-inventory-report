41ec73c4da8b83d2c6df53f917ff54859f890db9
Victor Hugo Baum Schlichting <victorgow2@gmail.com>
2022-08-12 16:23:31 -0300

    test: testing product class report

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,20 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    produto = Product(
+      1,
+      "Borracha",
+      "Papelaria Solar",
+      "2021-07-04",
+      "2029-02-09",
+      "FR48",
+      "Ao abrigo de luz solar"
+    )
+    assert produto.__repr__() == (
+        "O produto Borracha"
+        " fabricado em 2021-07-04"
+        " por Papelaria Solar com validade"
+        " até 2029-02-09"
+        " precisa ser armazenado Ao abrigo de luz solar."
+    )
