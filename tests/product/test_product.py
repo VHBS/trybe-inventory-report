71a4987b4ca1c704a15e33bf46fac60e4d4b8575
Victor Hugo Baum Schlichting <victorgow2@gmail.com>
2022-08-12 15:22:02 -0300

    test: testing attributes of the Product class

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,20 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    produto = Product(
+      1,
+      "Borracha",
+      "Papelaria Solar",
+      "2021-07-04",
+      "2029-02-09",
+      "FR48",
+      "Ao abrigo de luz solar"
+    )
+    assert produto.id == 1
+    assert produto.nome_do_produto == "Borracha"
+    assert produto.nome_da_empresa == "Papelaria Solar"
+    assert produto.data_de_fabricacao == "2021-07-04"
+    assert produto.data_de_validade == "2029-02-09"
+    assert produto.numero_de_serie == "FR48"
+    assert produto.instrucoes_de_armazenamento == "Ao abrigo de luz solar"
